097c9407e84da0dc20d453f580e3097995394564
Martín Figueroa Padilla <martnfigueroa21@gmail.com>
2019-02-13 22:01:49 -0600

    Avances botella original + sugerencias

---
+++ scripts/app.js
@@ -0,0 +1,5 @@
+import {MDCTextField} from '@material/textfield';
+import {MDCRipple} from '@material/ripple';
+
+const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
+const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
