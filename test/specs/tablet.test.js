7561b0d028a8010a47bcca7533357df9cdd6843e
Charlhye <carlosbmtz@gmail.com>
2019-02-13 21:26:41 -0600

    Tests

---
+++ test/specs/tablet.test.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+
+describe("Tablet Original Visible", () => {
+
+});
+
+describe("Tablet Places Visible", () => {
+
+});
+
+describe("Tablet Form Visible", () => {
+
+});
