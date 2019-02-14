620e8d2e9b70277363670d7482624f71f02fc076
Charlhye <carlosbmtz@gmail.com>
2019-02-13 20:38:50 -0600

    Webpack

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const autoprefixer = require('autoprefixer');
+
+module.exports = [{
+    entry: ['./styles/app.scss', './scripts/app.js'],
+    output: {
+        // This is necessary for webpack to compile
+        // But we never use style-bundle.js
+        filename: 'aplicacion.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'styles.css',
+                        },
+                    },
+                    { loader: 'extract-loader' },
+                    { loader: 'css-loader' },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: () => [autoprefixer()]
+                        }
+                    },
+                    { loader: 'sass-loader' ,
+                        options: {
+                            includePaths: ['./node_modules']
+                        }
+                    },
+                ]
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015'],
+                    plugins: ['transform-object-assign']
+                },
+            }
+        ]
+    },
+}];
