b9d0099df718ab0dc6078d77a56eeeaff4a51012
Charlhye <carlosbmtz@gmail.com>
2019-02-20 20:12:31 -0600

    Tablet & PC Tests

--- test/specs/tablet.test.js
+++ test/specs/tablet.test.js
@@ -1,13 +1,25 @@
 var assert = require('assert');
 
-describe("Tablet Original Visible", () => {
-
+describe("Tablet Original Visible", function(done) {
+    browser.url('/');
+    var verifyDiv = $('#verify-container');
+    var verifyFrameDisplay = verifyDiv.getCSSProperty('display');
+    assert.equal(verifyFrameDisplay.value, 'none');
+    browser.call(done);
 });
 
-describe("Tablet Places Visible", () => {
-
+describe("Tablet Places Visible", function(done) {
+    browser.url('/');
+    var placesDiv = $('#places-container');
+    var placesFrameDisplay = placesDiv.getCSSProperty('display');
+    assert.equal(placesFrameDisplay.value, 'none');
+    browser.call(done);
 });
 
-describe("Tablet Form Visible", () => {
-
+describe("Tablet Form Visible", function(done) {
+    browser.url('/');
+    var commentsDiv = $('#comments-container');
+    var commentsFrameDisplay = commentsDiv.getCSSProperty('display');
+    assert.equal(commentsFrameDisplay.value, 'block');
+    browser.call(done);
 });
