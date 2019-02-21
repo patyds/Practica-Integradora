6e2d1cac84a19aa8a3843c2bdf6f02c2c58be6fa
Martín Figueroa Padilla <martnfigueroa21@gmail.com>
2019-02-20 20:38:57 -0600

    Se esconden y todo

--- scripts/app.js
+++ scripts/app.js
@@ -3,3 +3,7 @@ import {MDCRipple} from '@material/ripple';
 
 const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
 const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
+const nameField = new MDCTextField(document.querySelector('.name-field'));
+const emailField = new MDCTextField(document.querySelector('.email-field'));
+const idField = new MDCTextField(document.querySelector('.id-field'));
+// const commentField = new MDCTextField(document.querySelector('.comment-field'));
